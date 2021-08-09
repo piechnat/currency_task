c71235208cd62cc61d40aaa6538f152a185fd5b2
piechnat <piechnat@interia.pl>
2021-08-09 15:55:03 +0200

    Add update route to NbpController

--- src/Controller/NbpController.php
+++ src/Controller/NbpController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CurrencyRepository;
 use App\Service\NbpApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class NbpController extends AbstractController
 {
     /**
-     * @Route("/nbp", name="nbp")
+     * @Route("/nbp", name="nbp_index")
      */
     public function index(NbpApiService $nbp): Response
     {
         return $this->render('nbp/index.html.twig', [
             'result' => $nbp->getExchangeRates('A'),
+        ]);
+    }
+
+    /**
+     * @Route("/nbp/update", name="nbp_update")
+     */
+    public function update(CurrencyRepository $currencyRepo, NbpApiService $nbp): Response
+    {
+        $currencyRepo->updateExchangeRates($nbp->getExchangeRates('A'));
+
+        return $this->render('nbp/update.html.twig', [
+            'result' => '',
         ]);
     }
 }
